f037ab9dd6ae87483ff4d5668c5aee3ad0f23498
danielvancura <danielvancura@gmail.com>
2018-03-02 16:09:31 -0800

    new commit

--- controllers/music.js
+++ controllers/music.js
@@ -8,7 +8,7 @@ router.get('/api', function(req, res) {
   var musicUrl = 'http://ws.audioscrobbler.com/2.0/?api_key=' + process.env.LAST_FM_KEY + '&format=json&method=artist.getsimilar&artist=' + artistName + '&limit=10';
   request(musicUrl, function(error, response, body) {
     body = JSON.parse(body);
-    res.render('show', {artistName: req.query.artist, similarArtists: body.similarartists.artist});
+    res.render('show', {artistName: artistName, similarArtists: body.similarartists.artist});
   });
 });
 
