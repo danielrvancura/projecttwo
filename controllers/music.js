8ccafe7febf1e9dce4e3b083dd7718de4dec2436
danielvancura <danielvancura@gmail.com>
2018-03-02 16:42:16 -0800

    Working locally 100%

--- controllers/music.js
+++ controllers/music.js
@@ -7,11 +7,11 @@ router.get('/api', function(req, res) {
   var artistName = req.query.artist;
   var musicUrl = 'http://ws.audioscrobbler.com/2.0/?api_key=' + process.env.LAST_FM_KEY + '&format=json&method=artist.getsimilar&artist=' + artistName + '&limit=10';
   request(musicUrl, function(error, response, body) {
-    // body = JSON.parse(body);
-    var similars = body.similarartists.artist;
-    console.log(similars);
+    body = JSON.parse(body);
+    // var similars = body.similarartists.artist;
+    console.log(body.similarartists.artist);
     // console.log(body.similarartists.artist);
-    res.render('show', {artistName: artistName, similarArtists: similars});
+    res.render('show', {artistName: artistName, similarArtists: body.similarartists.artist});
   });
 });
 
