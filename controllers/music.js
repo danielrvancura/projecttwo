802dd026ae3fbd38a91a21be52c0b047904d94d9
danielvancura <danielvancura@gmail.com>
2018-03-02 16:48:10 -0800

    Checking API URL

--- controllers/music.js
+++ controllers/music.js
@@ -6,6 +6,7 @@ var request = require('request');
 router.get('/api', function(req, res) {
   var artistName = req.query.artist;
   var musicUrl = 'http://ws.audioscrobbler.com/2.0/?api_key=' + process.env.LAST_FM_KEY + '&format=json&method=artist.getsimilar&artist=' + artistName + '&limit=10';
+  console.log(musicUrl);
   request(musicUrl, function(error, response, body) {
     body = JSON.parse(body);
     // var similars = body.similarartists.artist;
