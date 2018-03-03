75a6b855ab71d7228edf700c21ffeed9870f148e
danielvancura <danielvancura@gmail.com>
2018-03-02 16:30:54 -0800

    Removing the parse of the return

--- controllers/music.js
+++ controllers/music.js
@@ -7,7 +7,7 @@ router.get('/api', function(req, res) {
   var artistName = req.query.artist;
   var musicUrl = 'http://ws.audioscrobbler.com/2.0/?api_key=' + process.env.LAST_FM_KEY + '&format=json&method=artist.getsimilar&artist=' + artistName + '&limit=10';
   request(musicUrl, function(error, response, body) {
-    body = JSON.parse(body);
+    // body = JSON.parse(body);
     var similars = body.similarartists.artist;
     console.log(similars);
     // console.log(body.similarartists.artist);
