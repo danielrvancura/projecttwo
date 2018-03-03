e3570bd652f5025f1f4e9d05bd4e3f451e38aa46
danielvancura <danielvancura@gmail.com>
2018-03-02 16:44:54 -0800

    Troubleshooting heroku

--- controllers/music.js
+++ controllers/music.js
@@ -9,7 +9,7 @@ router.get('/api', function(req, res) {
   request(musicUrl, function(error, response, body) {
     body = JSON.parse(body);
     // var similars = body.similarartists.artist;
-    console.log(body.similarartists.artist);
+    console.log(body);
     // console.log(body.similarartists.artist);
     res.render('show', {artistName: artistName, similarArtists: body.similarartists.artist});
   });
