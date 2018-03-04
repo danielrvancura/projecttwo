d9358713799454b1a8e56963ea31f48a527132d6
danielvancura <danielvancura@gmail.com>
2018-03-04 12:01:51 -0800

    3/4 12:01pm

--- public/js/index.js
+++
@@ -1,6 +0,0 @@
-// $( document ).ready(function() {
-//   $( ".favorites" ).click(function() {
-//     console.log("Yes we are hittin it");
-//     $( ".favorites" ).css('background-color', 'green');
-//   });
-// });
